749712d60c82d5d07c0421ddfc148c1f10e5302e
Messenils <Nilssol@gmail.com>
2026-07-05 18:14:15 +0200

    globals.cs

--- Master/NucleusGaming/Globals.cs
+++ Master/NucleusGaming/Globals.cs
@@ -9,18 +9,18 @@ namespace Nucleus.Gaming
 {
     public static class Globals
     {
-        public const string Version = "2.4.2";
+        public const string Version = "2.4.1";
 
         public static readonly IniFile ini = new IniFile(Path.Combine(Directory.GetCurrentDirectory(), "Settings.ini"));
 
-        //returns theme path(current theme folder)main
+        //returns theme path(current theme folder)
         public static string ThemeFolder => Path.Combine(Application.StartupPath, $@"gui\theme\{App_Misc.Theme}\");
 
         //returns theme.ini file(current theme)
         public static IniFile ThemeConfigFile => new IniFile(Path.Combine(ThemeFolder, "theme.ini"));
 
         public static string GameProfilesFolder => Path.Combine(Application.StartupPath, $"game profiles");
-       
+
         public static readonly int NucleusMaxPlayers = 32;
 
         public static string NucleusInstallRoot => Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
